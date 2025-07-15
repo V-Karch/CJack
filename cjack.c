927829e7cd351f668a1af100311b99e9f07d9701
V-Karch <luna.karch05@gmail.com>
2025-07-15 14:43:21 -0400

    tested function calculation for hand value

--- cjack.c
+++ cjack.c
@@ -4,32 +4,18 @@ int main(void) {
     srand(time(NULL)); // Set random seed each time program is run
 
     Card* deck = create_deck();
-    printf("Unshuffled Deck:\n");
-    display_deck(deck);
-
     shuffle_deck(deck);
-    printf("\nShuffed Deck:\n");
-    display_deck(deck);
 
     Hand* player_hand = draw_player_hand(deck);
     Hand* dealer_hand = draw_dealer_hand(deck);
 
     printf("\nInitial Player Hand:\n");
+    printf("Hand Value: %d\n", calculate_hand_value(player_hand));
     display_hand(player_hand);
 
     printf("\nInitial Dealer Hand:\n");
+    printf("hand Value: %d\n", calculate_hand_value(dealer_hand));
     display_hand(dealer_hand);
-
-    printf("\nDeck after initial draws\n");
-    display_deck(deck);
-
-
-    draw_next_card_to_hand(player_hand, deck);
-    printf("\nTesting drawing next card to player hand\n");
-    display_deck(deck);
-
-    printf("\nPlayer hand after drawing one card\n");
-    display_hand(player_hand);
 
     free_hand(player_hand);
     free_hand(dealer_hand);
