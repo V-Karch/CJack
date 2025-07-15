8ec5e525c69f2ca487ec4c3f0e408922460239b6
V-Karch <luna.karch05@gmail.com>
2025-07-15 15:51:40 -0400

    added test for matching string on drawn queen of spades

--- tests/test_card.c
+++ tests/test_card.c
@@ -20,7 +20,7 @@ bool test_card_create(void) {
     return true;
 }
 
-bool test_card_string_match_undrawn_1_hearts(void) {
+bool test_card_string_match_undrawn_ace_hearts(void) {
     // Setup
     Card card = {1, false, HEARTS};
 
@@ -36,14 +36,31 @@ bool test_card_string_match_undrawn_1_hearts(void) {
     return result;
 }
 
+bool test_card_string_match_drawn_queen_spades(void) {
+    // Setup
+    Card card = {12, true, SPADES};
+
+    // Expected
+    char* expected_string = "\033[38;2;116;117;255mCard{queen, drawn, spades}\033[0m";
+
+    // Actual
+    char* actual_string = card_to_string(card);
+
+    bool result = strcmp(expected_string, actual_string) == 0;
+
+    free(actual_string);
+    return result;
+}
+
 int main(void) {
     printf("Running tests for card functionality...\n");
 
     Test card_create = {"test_card_create()", *test_card_create};
-    Test card_string_match_undrawn_1_hearts = {"test_card_string_match_undrawn_1_hearts()", *test_card_string_match_undrawn_1_hearts};
+    Test card_string_match_undrawn_ace_hearts = {"test_card_string_match_undrawn_ace_hearts()", *test_card_string_match_undrawn_ace_hearts};
+    Test card_string_match_drawn_queen_spades = {"test_card_string_match_drawn_queen_spades()", *test_card_string_match_drawn_queen_spades};
 
     Test tests[] = {
-        card_create, card_string_match_undrawn_1_hearts
+        card_create, card_string_match_undrawn_ace_hearts, card_string_match_drawn_queen_spades
     };
 
     run_tests(tests, sizeof(tests) / sizeof(Test));
