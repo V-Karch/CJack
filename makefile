1ba4629b59d6458c2af40d4eae9030982a6ec524
V-Karch <luna.karch05@gmail.com>
2025-07-15 15:46:06 -0400

    Improving upon testing

--- makefile
+++ makefile
@@ -2,7 +2,7 @@ cjack:
 	gcc cjack.c models/card.c models/deck.c models/hand.c game/blackjack.c -o cjack
 
 test:
-	gcc tests/*.c -o cjack_tests
+	gcc tests/*.c models/*.c -o cjack_tests
 	./cjack_tests
 
 clean:
--- tests/testing_utils.h
+++ tests/testing_utils.h
@@ -9,10 +9,20 @@ typedef struct {
     bool (*test)(void);
 } Test;
 
-void run_test(Test* test) {
-    if (test->test()) {
-        printf("\033[38;2;116;211;255m[%s] Passed!\033[0m\n", test->name);
+void run_test(Test test) {
+    if (test.test()) {
+        printf("\033[38;2;116;211;255m[%s] Passed!\033[0m\n", test.name);
     } else {
-        printf("\033[38;2;255;116;116m[%s] Failed!\033[0m\n", test->name);
+        printf("\033[38;2;255;116;116m[%s] Failed!\033[0m\n", test.name);
+    }
+}
+
+void run_tests(Test* tests, int length) {
+    for (int i = 0; i < length; i++) {
+        if (tests[i].test()) {
+            printf("\033[38;2;116;211;255m[%d/%d][%s] Passed!\033[0m\n", i + 1, length, tests[i].name);
+        } else {
+            printf("\033[38;2;255;116;116m[%d/%d][%s] Failed!\033[0m\n", i + 1, length, tests[i].name);
+        }
     }
 }
