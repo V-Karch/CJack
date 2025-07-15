f0e540455edb9a289bae2bbb9a5692cc99a7d899
V-Karch <luna.karch05@gmail.com>
2025-07-15 15:59:33 -0400

    added some extra tests, will have to improve upon how tests work later

--- tests/test_card.c
+++ tests/test_card.c
@@ -52,15 +52,41 @@ bool test_card_string_match_drawn_queen_spades(void) {
     return result;
 }
 
+bool test_suit_to_color_diamonds(void) {
+    return strcmp("\033[38;2;255;177;116m", suit_to_color(DIAMONDS)) == 0;
+}
+
+bool test_suit_to_color_clubs(void) {
+    return strcmp("\033[38;2;116;211;255m", suit_to_color(CLUBS)) == 0;
+}
+
+bool test_suit_to_color_spades(void) {
+    return strcmp("\033[38;2;116;117;255m", suit_to_color(SPADES)) == 0;
+}
+
+bool test_suit_to_color_hearts(void) {
+    return strcmp("\033[38;2;255;116;116m", suit_to_color(HEARTS)) == 0;
+}
+
 int main(void) {
     printf("Running tests for card functionality...\n");
 
     Test card_create = {"test_card_create()", *test_card_create};
     Test card_string_match_undrawn_ace_hearts = {"test_card_string_match_undrawn_ace_hearts()", *test_card_string_match_undrawn_ace_hearts};
     Test card_string_match_drawn_queen_spades = {"test_card_string_match_drawn_queen_spades()", *test_card_string_match_drawn_queen_spades};
+    Test suit_to_color_diamonds = {"test_suit_to_color_diamonds()", *test_suit_to_color_diamonds};
+    Test suit_to_color_hearts = {"test_suit_to_color_hearts()", *test_suit_to_color_hearts}; 
+    Test suit_to_color_clubs = {"test_suit_to_color_clubs()", *test_suit_to_color_clubs};
+    Test suit_to_color_spades = {"test_suit_to_color_spades()", *test_suit_to_color_spades};
 
     Test tests[] = {
-        card_create, card_string_match_undrawn_ace_hearts, card_string_match_drawn_queen_spades
+        card_create, 
+        card_string_match_undrawn_ace_hearts, 
+        card_string_match_drawn_queen_spades, 
+        suit_to_color_diamonds,
+        suit_to_color_hearts,
+        suit_to_color_clubs,
+        suit_to_color_spades,
     };
 
     run_tests(tests, sizeof(tests) / sizeof(Test));
